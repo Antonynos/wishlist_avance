cd5910f2840fe1bc1e26c851fd64f423a0b74e0e
Antonynos <dias.antonioa@gmail.com>
2024-08-27 06:10:17 -0300

    adição de seeds

--- backend_wishlist/database/seeders/DatabaseSeeder.php
+++ backend_wishlist/database/seeders/DatabaseSeeder.php
@@ -19,5 +19,25 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => 12345,
         ]);
+
+        DB::table('produtos')->insert([
+            'nome' => 'Colher',
+            'foto' => 'https://images.pexels.com/photos/2062687/pexels-photo-2062687.jpeg',
+        ]);
+
+        DB::table('produtos')->insert([
+            'nome' => 'Faca',
+            'foto' => 'https://images.pexels.com/photos/237996/pexels-photo-237996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ]);
+
+        DB::table('produtos')->insert([
+            'nome' => 'Canivete',
+            'foto' => 'https://images.pexels.com/photos/1000836/pexels-photo-1000836.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ]);
+
+        DB::table('produtos')->insert([
+            'nome' => 'Escova',
+            'foto' => 'https://images.pexels.com/photos/4202924/pexels-photo-4202924.jpeg',
+        ]);
     }
 }
